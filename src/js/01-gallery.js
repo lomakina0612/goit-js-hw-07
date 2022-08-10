5d66b1b927c1bb342c1b41bf9791d4ada2e836b2
Elena Lomakina <lomakina0612@gmail.com>
2022-08-11 00:07:45 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,15 +7,15 @@ const galleryContainer = document.querySelector(".gallery");
 const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
-console.log(galleryItemsMarkup);
 
+galleryContainer.addEventListener('click', onGalleryClick);
 
 
 function createGalleryItemsMarkup(items) {
   return items.map(({description, original, preview}) => {
     return `
      <div class="gallery__item">
-        <a class="gallery__link" href="${original}" target="_blank" rel="nofollow noopener noreferrer">
+        <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
             src="${preview}"
@@ -27,4 +27,21 @@ function createGalleryItemsMarkup(items) {
   `;
   }).join('');
   
+}
+
+function onGalleryClick(e) {
+  e.preventDefault(); 
+  const isGalleryImageEl = e.target.classList.contains('gallery__image');
+  if (!isGalleryImageEl) {
+    return;
+  }  
+  const currentImage = e.target;
+  const currentItem = currentImage.closest(".gallery__item");
+  const currentImageSource = currentImage.dataset.source;
+  
+  const instance = basicLightbox.create(`
+      <img src="${currentImageSource}" width="800" height="600">
+  `)
+
+  instance.show()
 }
