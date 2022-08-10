cb252cc3739bd9dff07c08710fa0d637e3e85740
Elena Lomakina <lomakina0612@gmail.com>
2022-08-10 16:44:54 +0200

    Create and render markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+console.log(galleryItemsMarkup);
+
+
+
+function createGalleryItemsMarkup(items) {
+  return items.map(({description, original, preview}) => {
+    return `
+     <div class="gallery__item">
+        <a class="gallery__link" href="${original}" target="_blank" rel="nofollow noopener noreferrer">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+  `;
+  }).join('');
+  
+}
